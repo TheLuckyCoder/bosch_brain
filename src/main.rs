cac2f4f7bcef5029c9025865974606a4f9b81889
Szilagyi David <thetropper26@gmail.com>
2023-03-08 14:58:08 +0200

    Added run command history

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code)]
 
 use env_logger::Env;
+use std::fs::OpenOptions;
+use std::io::BufRead;
+use std::path::Path;
+use std::thread::sleep;
 use tokio::task;
 
 use crate::serial::Message;
@@ -32,11 +36,34 @@ async fn main() -> std::io::Result<()> {
         .init();
 
     // let track = track::get_track();
-    task::spawn(async move {
-        if let Err(e) = server::steering_wheel::run_steering_wheel_server().await {
-            log::error!("Steering wheel server error: {e}");
+
+    if true {
+        task::spawn(async move {
+            if let Err(e) = server::steering_wheel::run_steering_wheel_server().await {
+                log::error!("Steering wheel server error: {e}");
+            }
+        });
+    } else if Path::new("commands_history.psv").exists() {
+        let file = OpenOptions::new().read(true).open("commands_history.psv")?;
+
+        // read all lines from file and store them in a Vec
+        let lines: Vec<_> = std::io::BufReader::new(file)
+            .lines()
+            .map(|l| l.unwrap())
+            .collect();
+
+        for line in lines {
+            let mut split = line.split('|');
+            let time = split.next().unwrap();
+            let message = split.next().unwrap();
+
+            serial::send_blocking(Message::Raw(message.to_string()))?;
+            //sleep for time milliseconds
+            sleep(std::time::Duration::from_millis(
+                time.parse::<u64>().unwrap(),
+            ));
         }
-    });
+    }
 
     brain::start_brain();
 
