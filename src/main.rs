0f61542a1bfafe71559fd216ce7c7db53d4f0294
David Szilagyi <thetropper26@gmail.com>
2023-08-24 18:49:52 +0300

    Updated Types, Implemented Panic, User Input Thread

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::io;
-use tokio::io::AsyncBufReadExt;
-use tokio::io::BufReader;
+use std::sync::{Arc, Mutex};
 use hc_sr04::{HcSr04, Unit};
 use linux_embedded_hal::{Delay, I2cdev};
 use pwm_pca9685::{Address, Channel, Pca9685};
-use std::time::{SystemTime, UNIX_EPOCH, Duration};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 struct MotorSettings {
     bonnet_channel: Channel,
@@ -30,15 +29,14 @@ const DC_MOTOR: MotorSettings = MotorSettings {
     percentage_maximum: 13.0,
 };
 
-fn map_from_percentage_to_12_bit_int(input: u64) -> u64 {
-    // Maps an input number that is between 0-100 (percentage) to 0-4096
-    // If the input is smaller than 0 or bigger than 100 it gives equivalent to it
+fn map_from_percentage_to_12_bit_int(input: f64) -> u64 {
+    // Maps an input floating-point number that is between 0.0-100.0 (percentage) to 0-4096
 
-    // Ensure input is within the 0-100 range
-    let clamped_input = input.clamp(0, 100);
+    // Ensure input is within the 0.0-100.0 range
+    let clamped_input = input.clamp(0.0, 100.0);
 
     // Map clamped_input to the 0-4096 range
-    ((clamped_input * 4096) / 100) as u64
+    ((clamped_input * 40.96) as u64)
 }
 
 pub fn set_motor_input(pwm: &mut Pca9685<I2cdev>, input: f64, motor_settings: MotorSettings) {
@@ -46,26 +44,24 @@ pub fn set_motor_input(pwm: &mut Pca9685<I2cdev>, input: f64, motor_settings: Mo
     //If the input is smaller than -1 or bigger than 1 it gives equivalent to it (percentage_minimum/maximum)
     let clamped_input = input.clamp(-1.0, 1.0);
 
-    let motor_input = match clamped_input {
+    let motor_input_percentage:f64 = match clamped_input {
         0.0 => {
-            motor_settings.percentage_middle as i64
+            motor_settings.percentage_middle
         }
         -1.0..=0.0 => {
-            let normalized_input = (clamped_input + 1.0) / 1.0;
-            (normalized_input * (motor_settings.percentage_middle - motor_settings.percentage_minimum)
-                + motor_settings.percentage_minimum) as i64
+            (clamped_input * (motor_settings.percentage_middle - motor_settings.percentage_minimum)
+                + motor_settings.percentage_minimum)
         }
         0.0..=1.0 => {
-            let normalized_input = (clamped_input + 1.0) / 1.0;
-            (normalized_input * (motor_settings.percentage_maximum - motor_settings.percentage_middle)
-                + motor_settings.percentage_middle) as i64
+            (clamped_input * (motor_settings.percentage_maximum - motor_settings.percentage_middle)
+                + motor_settings.percentage_middle)
         }
         _ => {
-            motor_settings.percentage_middle as i64
+            motor_settings.percentage_middle
         }
     };
 
-    pwm.set_channel_on_off(motor_settings.bonnet_channel, 0, motor_input as u16).unwrap();
+    pwm.set_channel_on_off(motor_settings.bonnet_channel, 0, map_from_percentage_to_12_bit_int(motor_input_percentage) as u16).unwrap();
 }
 
 
@@ -117,10 +113,20 @@ async fn main() -> anyhow::Result<()> {
         Some(20_f32) // Ambient temperature (if `None` defaults to 20.0C)
     )?;
 
-    let time_step = 100;
+    std::panic::set_hook(Box::new(|panic_info| {
+        println!("Panic occurred: {:?}", panic_info);
 
-    let stdin = io::stdin();
-    let mut reader = BufReader::new(stdin);
+        pwm.set_channel_on_off(Channel::C4, 0, map_from_percentage_to_12_bit_int(DC_MOTOR.percentage_middle) as u16).unwrap();
+        pwm.set_channel_on_off(Channel::C15, 0, map_from_percentage_to_12_bit_int(STEPPER_MOTOR.percentage_middle) as u16).unwrap();
+
+        let _dev = pwm.destroy(); // Get the I2C device back
+    }));
+
+    let mut steering_percentage = 0f64;
+
+    // Create an Arc and Mutex to share the user input between threads
+    let user_input = Arc::new(Mutex::new(None));
+
 
     loop {
         let now = SystemTime::now();
@@ -130,16 +136,40 @@ async fn main() -> anyhow::Result<()> {
         let nanos = duration.subsec_nanos();
         println!("Timestamp: {} seconds {} nanoseconds", seconds, nanos);
 
-        let steering_percentage = 0u64;
-        let speed_percentage = 0u64;
+        let input_thread = {
+            let user_input = Arc::clone(&user_input);
+            std::thread::spawn(move || {
+                loop {
+                    let mut input = String::new();
+                    println!("Enter a number between 0 and 100:");
+                    io::stdin().read_line(&mut input).expect("Failed to read line");
 
-        // Read input from stdin without blocking
-        // let mut input = String::new();
-        // if let Err(_) = tokio::time::timeout(std::time::Duration::from_secs(1), reader.read_line(&mut input)).await {
-        //     pwm.set_channel_on_off(Channel::C15, 0, map_from_percentage_to_12_bit_int(steering_percentage) as u16).unwrap();
-        //     pwm.set_channel_on_off(Channel::C4, 0, map_from_percentage_to_12_bit_int(speed_percentage) as u16).unwrap();
-        //     continue;
-        // }
+                    // Parse the input as a f64
+                    let parsed_input: Result<f64, _> = input.trim().parse();
+
+                    match parsed_input {
+                        Ok(num) if num >= 0.0 && num <= 100.0 => {
+                            // Input is valid, store it in the shared data
+                            let mut user_input = user_input.lock().unwrap();
+                            *user_input = Some(num);
+                            break; // Exit the loop
+                        }
+                        Ok(_) => println!("Input is not between 0.0 and 100.0 please try again."),
+                        Err(_) => println!("Invalid input. Please enter a number."),
+                    }
+                }
+            })
+        };
+
+        // Wait for the input thread to finish
+        input_thread.join().expect("Input thread panicked");
+
+        // Access the user input from the main thread
+        let locked_user_input = user_input.lock().unwrap();
+        steering_percentage = *locked_user_input.as_ref().expect("No valid input provided.");
+
+        pwm.set_channel_on_off(Channel::C15, 0, map_from_percentage_to_12_bit_int(steering_percentage) as u16).unwrap();
+        // pwm.set_channel_on_off(Channel::C4, 0, map_from_percentage_to_12_bit_int(speed_percentage) as u16).unwrap();
 
         // 0 ... 5000  ... 7000   ... 65536
         // 0 ... 312.5 ... 437.5  ... 4096
@@ -164,10 +194,6 @@ async fn main() -> anyhow::Result<()> {
         }
 
     }
-
-    let _dev = pwm.destroy(); // Get the I2C device back
-
-    Ok(())
 }
 
 // #![allow(dead_code)]
